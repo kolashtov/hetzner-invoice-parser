b86a5b3e2b18e8df52f9f77fdf9b45beb3f442e2
Andrey Kolashtov <kolashtov@gmail.com>
2024-07-10 22:25:59 +0200

    Added parser

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# hetzner-invoice-parser
+Hetzner Invoice Parser
+=======================
+
+Hetzner does not provide any cost calculation tools, so using this script you can easily calculate the total cost of your servers including addons.
+
+Usage
+-----
+
+Basic usage:
+```
+ruby ./hetzner-invoice-parser.rb <path_to_csv_invoice_file>
+```
+
+Sample output of the script:
+```
+Server #111111 project1.frontend1. Total price with addons: 44.5 euro
+Addons:
+  - 1x Upgrade to DDR4 ECC RAM: 1 x 5.5 = 5.5 euro
+  - 1x Primary IPv4 135.181.X.YYY: 1 x 1.7 = 1.7 euro
+  - 1x Additional IP address 135.181.X.ZZZ: 1 x 1.7 = 1.7 euro
+
+Server #222222 project1.backend1. Total price with addons: 62.3 euro
+Addons:
+  - 1x Upgrade from 64GB to 128GB DDR4 ECC RAM: 1 x 25.0 = 25.0 euro
+  - 1x Primary IPv4 135.181.X.QQQ: 1 x 1.7 = 1.7 euro
+
+Server #333333 project2.frontend2. Total price with addons: 80.2 euro
+Addons:
+  - 1x Primary IPv4 65.109.QQQ.LL: 1 x 1.7 = 1.7 euro
+  - 1x 10 Gbit Uplink: 1 x 42.9 = 42.9 euro
+
+Server #444444 project2.backend2. Total price with addons: 80.2 euro
+Addons:
+  - 1x Primary IPv4 65.109.DDD.RRR: 1 x 1.7 = 1.7 euro
+  - 1x 10 Gbit Uplink: 1 x 42.9 = 42.9 euro
+Global addons:
+- 1x 48-Port 10 Gbit switch: 1 x 342.1 = 342.1 euro
+====================================
+Total amount with global addons: 609.3
+====================================
+```
+
+
+Extended usage:
+```
+ruby ./hetzner-invoice-parser.rb <path_to_csv_invoice_file> <filter_pattern>
+```
+Example:
+```
+ruby ./hetzner-invoice-parser.rb hetzner.csv backend
+```
+
+
+Sample output of the script:
+```
+Server #222222 project1.backend1. Total price with addons: 62.3 euro
+Addons:
+  - 1x Upgrade from 64GB to 128GB DDR4 ECC RAM: 1 x 25.0 = 25.0 euro
+  - 1x Primary IPv4 135.181.X.QQQ: 1 x 1.7 = 1.7 euro
+
+Server #444444 project2.backend2. Total price with addons: 80.2 euro
+Addons:
+  - 1x Primary IPv4 65.109.DDD.RRR: 1 x 1.7 = 1.7 euro
+  - 1x 10 Gbit Uplink: 1 x 42.9 = 42.9 euro
+
+Global addons:
+- 1x 48-Port 10 Gbit switch: 1 x 342.1 = 342.1 euro
+
+====================================
+Servers are filtered by word: backend
+Total amount of filtered servers: 142.5 euro
+Amount is calculated without global addons!
+====================================
+```
+
+
+
---
+++ hetzner-invoice-parser.rb
@@ -0,0 +1,81 @@
+require 'csv'
+csv_file_name = ARGV[0]
+
+search_server_name = ARGV[1]
+
+
+ret = CSV.read(csv_file_name)
+servers = {}
+global_addons = []
+
+
+ret.each do |record|
+  next if record[0] != 'Servers'
+  if record[2][0] == '#'
+    #real server
+    server_id = record[2].match(/^#\d+/).to_s
+    server_name = record[2].match(/"([^"]+)"/)[1].to_s
+    servers[server_id] = {name: server_name, quantity: record[5].to_i, item_price: record[6].to_f.round(2), total_price: record[7].to_f.round(2), addons: []}
+  end
+end
+
+ret.each do |record|
+  next if record[0] != 'Servers'
+  if record[2][0] != '#'
+    #addons
+    if record[2].match(/Server (#\d+)/)
+      server_id = record[2].match(/Server (#\d+)/)[1].to_s
+    else
+      splitted = record[2].split(',')
+      if splitted.length == 1
+        splitted = record[2].split("\n")
+      end
+      addon_name = splitted[0]
+      global_addons << {name: addon_name, quantity: record[5].to_i, item_price: record[6].to_f.round(2), total_price: record[7].to_f.round(2)}
+      next
+    end
+    splitted = record[2].split(',')
+    if splitted.length == 1
+      splitted = record[2].split("\n")
+    end
+    addon_name = splitted[0]
+    servers[server_id][:addons] << {name: addon_name.gsub(/\n/, ' '), quantity: record[5].to_i, item_price: record[6].to_f, total_price: record[7].to_f}
+  end
+end
+
+total_sum = 0
+
+servers.each do |server_id, data|
+  next if search_server_name and !data[:name].include?(search_server_name)
+
+  server_total_cost = data[:total_price].to_f
+  data[:addons].each do |addon|
+    server_total_cost += addon[:total_price].to_f
+  end
+  total_sum += server_total_cost
+
+  puts "Server #{server_id} #{data[:name]}. Total price with addons: #{server_total_cost.round(2)} euro"
+  puts "Addons:" if data[:addons].any?
+  data[:addons].each do |addon|
+    puts "  - #{addon[:name]}: #{addon[:quantity].to_i} x #{addon[:item_price].round(2)} = #{addon[:total_price].round(2)} euro"
+  end
+  puts ""
+end
+
+puts "Global addons:" if global_addons.any?
+global_addons.each do |addon|
+  total_sum += addon[:total_price].round(2) if !search_server_name
+  puts "- #{addon[:name]}: #{addon[:quantity].to_i} x #{addon[:item_price].round(2)} = #{addon[:total_price].round(2)} euro"
+end
+
+puts ""
+puts "===================================="
+if search_server_name
+  puts "Servers are filtered by word: #{search_server_name}"
+  puts "Total amount by filtered servers: #{total_sum.round(2)} euro"
+  puts "Amount is calculated without global addons!"
+else
+  puts "Total amount with global addons: #{total_sum.round(2)} euro"
+end
+puts "===================================="
+
